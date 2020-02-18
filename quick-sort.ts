8a9f277331b0f0ca2aa19bc396a6f207eb0710d4
Ryan-Willpower <surasak.c.work@gmail.com>
2020-02-18 23:19:57 +0700

    init: first time uploaded

---
+++ quick-sort.ts
@@ -0,0 +1,39 @@
+let unsortedArray = [4, 1, 7, 6, 3, 5];
+
+function swap(array: number[], firstIndex: number, secondIndex: number) {
+  const temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
+
+function quickSort(array: number[], low: number, high: number) {
+  if (low < high) {
+    const pivot = partition(array, low, high);
+
+    // sort number before pi
+    quickSort(array, low, pivot - 1);
+
+    // sort number after pi
+    quickSort(array, pivot + 1, high);
+  }
+}
+
+function partition(array: number[], low: number, high: number) {
+  let i = low - 1;
+  let pivot = array[high];
+
+  for (let j = low; j <= high; j++) {
+    if (array[j] < pivot) {
+      i = i + 1;
+      swap(array, i, j);
+    }
+  }
+
+  swap(array, i + 1, high);
+
+  return i + 1;
+}
+
+quickSort(unsortedArray, 0, unsortedArray.length - 1);
+
+console.log(unsortedArray);
---
+++ readme.md
@@ -0,0 +1,1 @@
+change `.vscode/launch.json` at `configurations[0].program` to the file you want to debug for debugging.
