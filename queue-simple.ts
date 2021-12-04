da41ebe22f2993782c631f11e481b51eaf5940b2
Surasak Chaisurin <surasak.c.work@gmail.com>
2021-12-05 02:44:01 +0700

    chore: run code prettier

--- queue-simple.ts
+++ queue-simple.ts
@@ -1,87 +1,87 @@
-const SIZE = 5
+const SIZE = 5;
 
 class Queue {
-  private list = new Array(SIZE)
-  private front = -1
-  private rear = -1
+  private list = new Array(SIZE);
+  private front = -1;
+  private rear = -1;
 
   isEmpty() {
-    return this.front === -1
+    return this.front === -1;
   }
 
   isFull() {
-    return this.front === 0 && this.rear === SIZE - 1
+    return this.front === 0 && this.rear === SIZE - 1;
   }
 
   enqueue(data: number) {
     if (this.isFull()) {
-      console.log(`queue is full`)
-      return
+      console.log(`queue is full`);
+      return;
     }
 
-    if (this.front === -1) this.front = 0
-    this.rear += 1
-    this.list[this.rear] = data
+    if (this.front === -1) this.front = 0;
+    this.rear += 1;
+    this.list[this.rear] = data;
 
-    console.log(`insert ${data} to queue`)
+    console.log(`insert ${data} to queue`);
   }
 
   dequeue() {
     if (this.isEmpty()) {
-      console.log(`queue is empty`)
-      return
+      console.log(`queue is empty`);
+      return;
     }
 
-    const element = this.list[this.front]
+    const element = this.list[this.front];
 
     if (this.front >= this.rear) {
-      this.front = -1
-      this.rear = -1
+      this.front = -1;
+      this.rear = -1;
 
-      console.log(`queue is now empty`)
+      console.log(`queue is now empty`);
     } else {
-      this.front += 1
+      this.front += 1;
     }
 
-    console.log(`element ${element} is deleted`)
+    console.log(`element ${element} is deleted`);
   }
 
   display() {
     if (this.isEmpty()) {
-      console.log([])
-      return
+      console.log([]);
+      return;
     }
 
-    const stack = []
+    const stack = [];
 
     for (let i = this.front; i <= this.rear; i++) {
-      stack.push(this.list[i])
+      stack.push(this.list[i]);
     }
 
-    console.log(stack)
+    console.log(stack);
   }
 }
 
-const queue = new Queue()
+const queue = new Queue();
 
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
-queue.enqueue(4)
-queue.enqueue(5)
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(5);
 
-queue.display() // [1,2,3,4,5]
+queue.display(); // [1,2,3,4,5]
 
 // this will be skipped because queue is full
-queue.enqueue(6)
+queue.enqueue(6);
 
-queue.dequeue()
-queue.dequeue()
-queue.dequeue()
-queue.dequeue()
-queue.dequeue()
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
 
-queue.display() // []
+queue.display(); // []
 
 // this will be skipped because queue is empty
-queue.dequeue()
+queue.dequeue();
